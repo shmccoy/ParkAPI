61346a4420d19c2c83fa7d101bdc4e8a4d81a62a
Sean M <53903852+shmccoy@users.noreply.github.com>
2020-01-23 16:39:01 -0500

    update

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function formatQueryParams(params) {
     const queryItems = Object.keys(params).map(key => `${[encodeURIComponent(key)]}=${encodeURIComponent(params[key])}`);
     return queryItems.join('&');
